b2932f14179d4572d7981b10eb097dfe18280f21
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2022-08-05 19:59:24 +0900

    feat: add location model

--- back/locations/models.py
+++ back/locations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Location:
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="locations")
+    location_nickname = models.CharField(max_length=20)
+    latitude = models.FloatField(max_length=40)
+    longitude = models.FloatField(max_length=40)
