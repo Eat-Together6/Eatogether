d68b716c340297b013441b564f3891a09fbfec1e
qndn3tp <qndn3tp@gmail.com>
2022-08-07 17:30:55 +0900

    feat: 새 메뉴 작성 기본페이지 구성

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import CreateMenu from "./pages/CreateMenu/CreateMenu";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createMenu" element={<CreateMenu />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-//
 
 export default App;
