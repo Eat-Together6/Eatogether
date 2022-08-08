497151f8396aecc7064c377e04e9643676eae7a4
qndn3tp <qndn3tp@gmail.com>
2022-08-08 21:39:34 +0900

    feat: 세 메뉴 작성하기 페이지 수정

--- front/src/pages/CreateMenu/CreateMenu.js
+++ front/src/pages/CreateMenu/CreateMenu.js
@@ -1,4 +1,7 @@
-import React from "react";
+import Box from "@mui/material/Box";
+import BoardInput from "../../components/EtcItem/BoardInput";
+import UnstyledButtonsSimple from "../../components/EtcItem/BasicButton";
+import { Link } from "react-router-dom";
 
 const headerStyle = {
   marginTop: "60px",
@@ -8,53 +11,63 @@ const headerStyle = {
   height: "150px",
 };
 
-const Contents = {
+const Contents_one = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
 };
 
-const Contents_element = {
+const Contents_two = {
+  fontSize: "30px",
+  width: "100%",
   display: "flex",
+  marginRight: "60px",
+  flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
+};
+
+const btnWrapper = {
+  display: "flex",
+  alignItems: "center",
+  height: "200px",
+  justifyContent: "center",
+  marginLeft: "100px",
+};
+
+const orderMenu = {
+  display: "felx",
 };
 
 function CreateMenu() {
   return (
     <>
-      <div style={Contents}>
+      <Box>
         <div style={headerStyle}>
           <h1>새 메뉴 작성하기</h1>
         </div>
-        <div style={Contents_element}>
-          <h3>픽업 주소</h3>
-          <input type="text"></input>
+      </Box>
+      <Box style={Contents_one}>
+        <div style={Contents_two}>
+          <BoardInput class="addess" label={"픽업 주소"} name="address" />
+          <BoardInput label={"주문 희망 브랜드"} name="barnd" />
+          <div style={orderMenu}>
+            <BoardInput label={"주문 희망 메뉴, 가격"} name="menu" />
+          </div>
+          <BoardInput label={"주문 희망 시간"} name="time" />
+          <BoardInput label={"총 금액"} name="sum" />
+          <BoardInput label={"전달 사항"} name="description" />
+          <div id="btnWrap" style={btnWrapper}>
+            <Link style={{ textDecoration: "none", color: "white" }} to="/">
+              <UnstyledButtonsSimple label={"작성"} />
+            </Link>
+            <Link style={{ textDecoration: "none", color: "white" }} to="/">
+              <UnstyledButtonsSimple label={"작성 취소"} />
+            </Link>
+          </div>
         </div>
-        <div style={Contents_element}>
-          <h3>주문 희망 브랜드</h3>
-          <input type="text"></input>
-        </div>
-        <div style={Contents_element}>
-          <h3>주문 희망 메뉴, 가격</h3>
-          <input type="text"></input>
-          <input type="text"></input>
-          <button>메뉴 추가</button>
-        </div>
-        <div style={Contents_element}>
-          <h3>주문 희망 시간</h3>
-          <input type="text"></input>
-        </div>
-        <div style={Contents_element}>
-          <h3>총 금액</h3>
-          <input type="text"></input>
-        </div>
-        <div style={Contents_element}>
-          <button>작성</button>
-          <button>작성 취소</button>
-        </div>
-      </div>
+      </Box>
     </>
   );
 }
