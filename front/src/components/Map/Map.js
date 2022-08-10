3416d0941e875039b0c54ed9f7eed0faf7654b82
rkdcodus <rcd7275@naver.com>
2022-08-11 02:34:53 +0900

    feat: marker drag function, show new marker address / fix: create marker only one click

--- front/src/components/Map/Map.js
+++ front/src/components/Map/Map.js
@@ -7,11 +7,9 @@ import { PopperUnstyled } from "@mui/base";
 
 const Map = () => {
     const location = useGeolocation();
-    
     const {kakao} = window;
     const [map, setMap] = useState();
     const container = useRef();
-
     const options = {
         center: new kakao.maps.LatLng(location.coordinates.lat, location.coordinates.lng),
 		level: 4
@@ -22,17 +20,14 @@ const Map = () => {
     },[location]);
     
     //주소 검색 후 해당 위치로 지도 중심 옮기기
-
     const [address, setAddress] = useState();
     const [latLng, setLatLng] = useState({
         lat:0,
         lon:0
     });
+    const geocoder = new kakao.maps.services.Geocoder();
 
     const searchAndMoveByAddress = () => {
-        
-        const geocoder = new kakao.maps.services.Geocoder();
-        
         console.log('searchAndMoveByAddress :'+ address)
         geocoder.addressSearch(address, function(result, status){
             if(status === kakao.maps.services.Status.OK) {
@@ -41,9 +36,9 @@ const Map = () => {
                 setLatLng({
                     lat: coords.Ma,
                     lon: coords.La
-                }) // 주소 좌표로 변환하기
-                console.log("주소 검색 완료되었습니다⭕", latLng.lat, latLng.lon)
-                console.log('검색 후 : ', popup)
+                }) // 주소 좌표로 변환하기 아직 쓸일 없는거 같음
+                displayMarker(coords.Ma, coords.La)
+                console.log("주소 검색 완료되었습니다⭕")
             } else{
                 console.log("주소가 정확하지 않습니다❌")
             }
@@ -51,22 +46,36 @@ const Map = () => {
     };
     
     // 입력 주소로 마크 표시
-    const displayMarker = () => {
+    const displayMarker = (lat, lon) => {
         // 마커 이미지 파일 경로, 사이즈, 주소 좌표 일치시킬 이미지 좌표 옵션
         const imageSrc = NewMarker;
         const imageSize = new kakao.maps.Size(50,50);
         const imageOption = {offset: new kakao.maps.Point(20,50)};
-
         const markerImage = new kakao.maps.MarkerImage(imageSrc,imageSize,imageOption)
-        
-        const markerPosition = new kakao.maps.LatLng(latLng.lat, latLng.lon);
+        const markerPosition = new kakao.maps.LatLng(lat, lon);
         const marker = new kakao.maps.Marker({
             position: markerPosition,
-            image: markerImage
+            image: markerImage,
+            draggable: true
         });
         // console.log(markerPosition.latLng)
         marker.setMap(map);
+        // marker.setDraggable(true);
         console.log("마커가 표시되었습니다✅")
+
+        kakao.maps.event.addListener(marker, 'dragend', function() {
+            searchDetailAddrFromCoords(marker.getPosition(), function(result, status){
+                if(status === kakao.maps.services.Status.OK){
+                    setPopup(!popup)
+                    setAddress(result[0].road_address.address_name)
+                }
+            })
+        });
+    }
+    console.log("주소값 : ", address)
+    function searchDetailAddrFromCoords(coords, callback) {
+        // 좌표로 법정동 상세 주소 정보를 요청합니다
+        geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
     }
 
     const [popup, setPopup] = useState(false);
@@ -89,11 +98,7 @@ const Map = () => {
                 <input 
                     type="button" 
                     className={NavStyle.imgBtn} 
-                    onClick={() => {
-                        displayMarker()
-                        searchAndMoveByAddress()
-                        }
-                    }
+                    onClick={searchAndMoveByAddress}
                 />
                 {popup && <Post setAddress={setAddress} />}
             </form>
