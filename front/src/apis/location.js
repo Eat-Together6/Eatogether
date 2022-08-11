4205b376d55ba5707c3139316491e782fc95d861
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2022-08-12 01:30:13 +0900

    docs: apis/locations 주석 추가

--- front/src/apis/location.js
+++ front/src/apis/location.js
@@ -1,11 +1,13 @@
 import apiClient from "../lib/apiClient"
 
+// path('locations/', views.LocationList.as_view())
 const getLocations = () => new Promise((resolve) => {
     resolve(apiClient
         .get('/locations/')
         .then(response => response.data));
 });
 
+// path('locations/', views.LocationList.as_view())
 const addLocation = async (name, lat, lon) => {
     return await apiClient.post('/locations/', {
         name: name,
