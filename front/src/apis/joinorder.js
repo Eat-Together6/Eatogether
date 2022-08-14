2560c35f3240510591cb71acddd11738f45a605d
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2022-08-14 20:28:25 +0900

    fix: front axios apis 주석 삭제

--- front/src/apis/joinorder.js
+++ front/src/apis/joinorder.js
@@ -2,10 +2,10 @@ const getJoinOrders = (id) => new Promise((resolve) => {
     resolve(apiClient
         .get(`http://localhost:8000/joinorders/${id}/`)
         .then((response) => response.data));
-}); // http://localhost:8000/apis/joinorders/{정수}
+});
 
 const addJoinOrder = async (order_id, follower, description) => {
   return await apiClient.post("http://localhost:8000/joinorders/", {order_id, follower, description});
-}; // http://localhost:8000/apis/joinorders/{정수}/locations/
+};
 
 export { getJoinOrders, addJoinOrder };
