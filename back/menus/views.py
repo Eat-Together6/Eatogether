a36971ab5e01e045cf957c44d26f8db585512713
charlesuu <tiger0192004@gmail.com>
2022-08-16 17:19:20 +0900

    menu뷰 get 함수 수정

--- back/menus/views.py
+++ back/menus/views.py
@@ -9,18 +9,18 @@ from menus.models import Menu
 from menus.serializers import MenuSerializer
 
 class MenuView(APIView):
-    def get(self, request):
+    def get(self, request): #postman 테스트 완료
         join_order_id = request.GET.get('join_order_id', None) 
         if join_order_id is None:
             menus = Menu.objects.all()
             serializer = MenuSerializer(menus, many=True)
             return Response(serializer.data)
-        else:
+        else: #postman 테스트 실패 : join_order_id가 주어져도 모든 menus를 받아옴.
             menu = Menu.objects.filter(join_order_id=join_order_id)
             serializer = MenuSerializer(menu, many=True)
             return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request):#postman 테스트 완료
         join_order = JoinOrder.objects.get(id=request.data['join_order'])
         menu = Menu.objects.create(join_order=join_order,
                                    menu_name=request.data['menu_name'],
@@ -29,28 +29,3 @@ class MenuView(APIView):
                                    )
         serializer = MenuSerializer(menu)
         return Response(serializer.data)
-
-class MenuDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Menu.objects.get(pk=pk)
-        except Menu.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        menus = self.get_object(pk)
-        serializer = MenuSerializer(menus, many=True)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        menu = self.get_object(pk)
-        serializer = MenuSerializer(menu, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        menu = self.get_object(pk)
-        menu.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
