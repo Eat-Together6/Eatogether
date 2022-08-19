8e7212c867b7f7842e2b59db6cf63a78b2103b71
rkdcodus <rcd7275@naver.com>
2022-08-19 23:27:27 +0900

    feat: connecting 확인하기

--- front/src/pages/Board/Board.js
+++ front/src/pages/Board/Board.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.js";
 import Post from "./Post";
 import { getOrderAndJoinOrders } from "api/order.js";
 
 const Board = () => {
+  const [orders, setOrder] = useState();
+
   const getAllOrder = async () => {
     await getOrderAndJoinOrders() //
       .then((res) => {
-        console.log("AA", res);
+        const listAllOrder = res.data.map((order) => {
+          return {
+            store: order.store,
+            address: order.location,
+            date: order.time.substring(0, 10),
+            time: order.time.substring(11, 19),
+            description: order.description,
+            join: order.join_order_list,
+          };
+        });
+        setOrder(listAllOrder);
       })
       .catch((e) => {
         console.log("error", e);
@@ -17,6 +29,7 @@ const Board = () => {
   useEffect(() => {
     getAllOrder();
   }, []);
+
   return (
     <>
       <div style={styles.background}>
